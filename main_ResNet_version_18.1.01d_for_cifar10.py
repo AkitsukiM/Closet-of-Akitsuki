84c04701d78b2b9935090c1fd595d10c452e659b
Marina Akitsuki <Gcy.27182@outlook.com>
2020-08-15 13:39:45 +0800

    updated on 2020/08/15

--- main_ResNet_version_18.1.01d_for_cifar10.py
+++ main_ResNet_version_18.1.01d_for_cifar10.py
@@ -1,40 +1,41 @@
-# ResNet version 18.1.01c for cifar10
-# 
-# 1.00b Update Information:
-# (i) Rewrite based on myResNet version 1.25a.
-# 
-# 1.01a Update Information:
-# (i) Change counting unit to iteration instead of epoch;
-# (ii) Add some information of ImageNet for reference.
+# ResNet version 18.1.01d for cifar10
 # 
 # Copyright (c) 2020 Marina Akitsuki. All rights reserved.
-# Date modified: 2020/08/12
+# Date modified: 2020/08/15
 # 
 
 datasetsMean = (0.4914, 0.4822, 0.4465) # (0.485, 0.456, 0.406) # datasetsMean for ImageNet
 datasetsStd = (0.2023, 0.1994, 0.2010) # (0.229, 0.224, 0.225) # datasetsStd for ImageNet
-batchSize = 100
 batchSizeTrain = 128
+batchSizeTest = 100
 
 blocks = (1, 2, 2, 2, 2) # 2 + 2 * (2 + 2 + 2 + 2) = 18
 ioplanes = (16, 16, 32, 64, 128)
-fcOut = 10 # 1000 # fcOut for ImageNet
-ConvBlock_kernel_size = 3 # 7 # ConvBlock_kernel_size for ImageNet
+fcOut = 10
+ConvBlock_kernel_size = 3
 ConvBlock_stride = 1
 
 iLearningRate = 0.1
 iMomentum = 0.9
 iWeightDecay = 1e-4
 iterationWhenPrintRunningLoss = 100
-iterationWhenCalcAccuracy = 1000
-iterationWhenLearningRateDacay = (32000, 64000)
-iterationWhenMissionAccomplished = 96000
+iterationWhenEvaluate = 1000
+iterationWhenLearningRateDacay = (32000, 48000)
+iterationWhenMissionAccomplished = 64000
 
 ROOT_PATH = "../../Dataset/"
 
+import sys
+import datetime
+
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
+
+import myResNet
 
 gpu = True if torch.cuda.is_available() else False
 device = torch.device("cuda" if gpu else "cpu")
@@ -43,7 +44,6 @@ print(gpu, device)
 transform = transforms.Compose([
     transforms.ToTensor(), transforms.Normalize(datasetsMean, datasetsStd)])
 
-# """
 transformTrain = transforms.Compose([
     transforms.RandomCrop(32, padding = 4),
     transforms.RandomHorizontalFlip(),
@@ -52,39 +52,10 @@ transformTrain = transforms.Compose([
 trainset = torchvision.datasets.CIFAR10(root = ROOT_PATH, train = True, download = True, transform = transformTrain)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size = batchSizeTrain, shuffle = True, num_workers = (2 if gpu else 0))
 
-traintestset = torchvision.datasets.CIFAR10(root = ROOT_PATH, train = True, download = True, transform = transform)
-traintestloader = torch.utils.data.DataLoader(traintestset, batch_size = batchSize, shuffle = False, num_workers = (2 if gpu else 0))
+trastset = torchvision.datasets.CIFAR10(root = ROOT_PATH, train = True, download = True, transform = transform)
+trastloader = torch.utils.data.DataLoader(trastset, batch_size = batchSizeTest, shuffle = False, num_workers = (2 if gpu else 0))
 testset = torchvision.datasets.CIFAR10(root = ROOT_PATH, train = False, download = True, transform = transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size = batchSize, shuffle = False, num_workers = (2 if gpu else 0))
-
-"""
-
-# Data Augmentation Strategy for ImageNet
-transformTrain = transforms.Compose([
-    transforms.Resize(256),
-    transforms.RandomCrop(224),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(), transforms.Normalize(datasetsMean, datasetsStd)])
-
-transformVal = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(), transforms.Normalize(datasetsMean, datasetsStd)])
-
-trainset = torchvision.datasets.ImageFolder(root = "/home/ubuntu/Workspace/Dataset/ILSVRC2012/train", transform = transformTrain)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size = batchSizeTrain, shuffle = True, num_workers = (2 if gpu else 0))
-
-valset = torchvision.datasets.ImageFolder(root = "/home/ubuntu/Workspace/Dataset/ILSVRC2012/val", transform = transformVal)
-valloader = torch.utils.data.DataLoader(valset, batch_size = batchSize, shuffle = False, num_workers = (2 if gpu else 0))
-# """
-
-import datetime
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import myResNet
-
-t1 = datetime.datetime.now()
+testloader = torch.utils.data.DataLoader(testset, batch_size = batchSizeTest, shuffle = False, num_workers = (2 if gpu else 0))
 
 net = myResNet.ResNet(myResNet.BasicBlock, blocks, ioplanes, fcOut, ConvBlock_kernel_size = ConvBlock_kernel_size)
 if gpu: net.to(device)
@@ -95,7 +66,7 @@ optimizer = optim.SGD(net.parameters(), lr = iLearningRate, momentum = iMomentum
 runningLoss = 0.0
 iterationCount = 0
 
-def calcAccuracy(loader):
+def toEvaluate(loader):
     set_correct = 0.0
     set_total = 0.0
 
@@ -106,16 +77,17 @@ def calcAccuracy(loader):
             else:
                 inputs, labels = data
 
-            # inputs = [inputs, inputs * 0] # for testing ComplexNet
             outputs = net(inputs)
             _, predictions = torch.max(outputs.data, 1)
-
-            for i in range(batchSize):
+            
+            for i in range(outputs.size(0)): # outputs.size(0) == labels.size(0) == batchSizeTest
                 set_total += 1
                 if predictions[i] == labels[i]: set_correct += 1
 
     set_accuracy = 100 * set_correct / set_total
     print("Accuracy of total: %d/%d = %.1f %%" % (set_correct, set_total, set_accuracy))
+
+t1 = datetime.datetime.now()
 
 while True:
     for i, data in enumerate(trainloader, 0):
@@ -126,7 +98,6 @@ while True:
 
         optimizer.zero_grad()
 
-        # inputs = [inputs, inputs * 0] # for testing ComplexNet
         outputs = net(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
@@ -139,12 +110,12 @@ while True:
 
         iterationCount += 1
 
-        if iterationCount % iterationWhenCalcAccuracy == 0:
+        if iterationCount % iterationWhenEvaluate == 0:
+            torch.save(net, "./ResNet18_iteration" + str(iterationCount) + ".pth") # temporary save
             print("")
             print("iteration %d:" % (iterationCount))
 
-            for loader in (traintestloader, testloader): calcAccuracy(loader)
-            # calcAccuracy(valloader) # valloader for ImageNet
+            for loader in (trastloader, testloader): toEvaluate(loader)
 
             t2 = datetime.datetime.now()
             print(t2 - t1)
@@ -155,4 +126,6 @@ while True:
                 params["lr"] = iLearningRate
 
         if iterationCount == iterationWhenMissionAccomplished:
-            exit(0)
+            torch.save(net, "./ResNet18.pth") # final save
+            sys.exit(0)
+
